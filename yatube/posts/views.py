3d23e24bc0462bfd09998b4e545c4608b0e3aa01
Алексеев Иван <alekseevivan@MacBook-Pro-Alekseev.local>
2021-12-27 12:10:12 +0300

    mend

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,26 @@
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title = "Это главная страница проекта Yatube"
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        "title": title,
+        "posts": posts
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Главная страница {slug}')
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'title': 'Группы',
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
